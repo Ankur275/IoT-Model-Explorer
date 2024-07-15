ab30fe3d547ddc281b64c52cbda8b86a3b34d515
Ankur <ankurchakrajit@gmail.com>
2024-07-15 18:51:14 +0530

    blog

---
+++ backend/Models/blog.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    content: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    tags: [{
+        type: String
+    }],
+    image: {
+        type: String,
+        trim: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    comments: [{
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }]
+}, { timestamps: true });
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
